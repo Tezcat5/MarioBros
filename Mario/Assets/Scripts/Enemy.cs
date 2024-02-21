14cea498990a6f7752c7274885e25516a8f0fd4b
Tezcat5 <vicpero96@gmail.com>
2024-02-21 19:26:03 +0100

    Initial commit

---
+++ Mario/Assets/Scripts/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+
+    public float enemySpeed = 5;
+
+    public float enemyDirection = 1;
+
+    public AudioClip deathSound;
+
+    public BoxCollider2D boxCollider;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rBody.velocity = new Vector2(enemyDirection * enemySpeed, rBody.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 3 || collision.gameObject.tag == "Goombas")
+        {
+            if(enemyDirection == 1)
+            {
+            enemyDirection = -1;
+            }
+            else if(enemyDirection == -1)
+            {
+                 enemyDirection = 1;
+            }
+        }
+
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    
+
+    public void GoombaDeath()
+    {    
+        source.PlayOneShot(deathSound);
+        boxCollider.enabled = false;
+        rBody.gravityScale = 0;
+        enemyDirection = 0;
+        Destroy(gameObject, 0.5f);
+    }
+
+
+
+}
+
+
